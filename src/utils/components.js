419034cd6eb47d0eff840b392d799cd9f4f3244c
Leon Alvarez Del Canto <leon@leon-design.es>
2020-07-12 14:00:43 +0200

    button componennt

--- src/utils/components.js
+++ src/utils/components.js
@@ -3,17 +3,17 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 100%;
-    @media (min-width: 576px) {
-        max-width: 576px;
+    @media (min-width: 640px) {
+        max-width: 640px;
     }
     @media (min-width: 768px) {
         max-width: 768px;
     }
-    @media (min-width: 992) {
-        max-width: 992px;
+    @media (min-width: 1024px) {
+        max-width: 1024px;
     }
-    @media (min-width: 1200px) {
-        max-width: 1200px;
+    @media (min-width: 1280px) {
+        max-width: 1280px;
     }
 `;
 
@@ -35,3 +35,24 @@ export const Heading = ({ level = 1, children, color, fontSize, fontWeight, clas
     >
         {children}
     </StyledHeading>
+
+export const Button = styled.button.attrs(({ type, secondary, size, rounded }) => ({
+    type: type || "button",
+    size: size || "normal",
+    rounded: rounded === true
+        ? 'rounded'
+        : rounded,
+    stylesTheme: secondary
+        ? 'secondary'
+        : 'primary',
+}))`
+    ${({ rounded, theme }) => rounded && `
+        border-radius: ${theme.borderRadius[rounded]};
+    `};
+    background-color: ${({ bgColor, theme, stylesTheme }) => bgColor || theme.buttons[stylesTheme].bgColor};
+    color: ${({ color, theme, stylesTheme }) => color || theme.buttons[stylesTheme].color};
+    font-size: ${({ fontSize, theme, size }) => fontSize || theme.buttonsSize[size].fontSize};
+    font-weight: ${({ fontWeight, theme, size }) => fontWeight || theme.buttonsSize[size].fontWeight};
+    padding: ${({ padding, theme, size }) => padding || theme.buttonsSize[size].padding};
+    margin: ${({ margin }) => margin || 0};
+`;
--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,48 +1,79 @@
 const headings = {
-    1: {
-        fontSize: '2rem',
-        fontWeight: 600,
-    },
-    2: {
-        fontSize: '2rem',
-        fontWeight: 600,
-    },
-    3: {
-        fontSize: '2rem',
-        fontWeight: 600,
-    },
-    4: {
-        fontSize: '2rem',
-        fontWeight: 600,
-    }
+  1: {
+    fontSize: '2rem',
+    fontWeight: 600,
+  },
+  2: {
+    fontSize: '2rem',
+    fontWeight: 600,
+  },
+  3: {
+    fontSize: '2rem',
+    fontWeight: 600,
+  },
+  4: {
+    fontSize: '2rem',
+    fontWeight: 600,
+  }
 }
 
 const lightTheme = {
-    transition: '0.37s',
-    mainBgColor: '#f9f9f9',
-    header: {
-        bgColor: '#f9f9f9',
-        textColor: '#05233b',
-        accentColor: '#3490dc'
-    },
+  transition: '0.37s',
+  mainBgColor: '#f9f9f9',
+  header: {
+    bgColor: '#f9f9f9',
     textColor: '#05233b',
-    accentColor: '#6128d3',
-    headings
+    accentColor: '#3490dc'
+  },
+  textColor: '#05233b',
+  accentColor: '#6128d3',
+  headings
 };
 
 const darkTheme = {
-    transition: '0.37s',
+  transition: '0.37s',
+  bgColor: '#05233b',
+  lightBgColor: '#f9f9f9',
+  header: {
     bgColor: '#05233b',
-    lightBgColor: '#f9f9f9',
-    header: {
-        bgColor: '#05233b',
-        textColor: '#f9f9f9',
-        accentColor: '#3490dc'
+    textColor: '#f9f9f9',
+    accentColor: '#3490dc'
+  },
+  textColor: '#05233b',
+  lightTextColor: '#f9f9f9',
+  accentColor: '#6128d3',
+  headings,
+  buttons: {
+    primary: {
+      bgColor: '#05233b',
+      color: '#f9f9f9',
     },
-    textColor: '#05233b',
-    lightTextColor: '#f9f9f9',
-    accentColor: '#6128d3',
-    headings,
+    secondary: {
+      bgColor: '#f9f9f9',
+      color: '#05233b',
+    },
+  },
+  buttonsSize: {
+    small: {
+      fontSize: '0.75rem',
+      fontWeight: 400,
+      padding: '0.25rem 0.75rem',
+    },
+    normal: {
+      fontSize: '1rem',
+      fontWeight: 500,
+      padding: '0.5rem 1rem',
+    }
+  },
+  borderRadius: {
+    rounded: '0.25rem'
+  },
+  screens: {
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+  }
 };
 
 export { darkTheme, lightTheme }
