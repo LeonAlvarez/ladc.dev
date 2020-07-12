9a6d4b3e991b772927752f3db241989c8b4643f5
Leon Alvarez Del Canto <leon@leon-design.es>
2020-07-13 01:41:15 +0200

    fix space

--- src/utils/mediaQueries.js
+++ src/utils/mediaQueries.js
@@ -1,32 +1,32 @@
 import { css } from 'styled-components';
 
 const mediaQueries = ({ theme }) => {
-    const getSizeFromBreakpoint = (breakpoint) => theme.screens[breakpoint];
+  const getSizeFromBreakpoint = (breakpoint) => theme.screens[breakpoint];
 
-    const lessThan = (breakpoint) => (...styles) => css`
+  const lessThan = (breakpoint) => (...styles) => css`
         @media (max-width: ${getSizeFromBreakpoint(breakpoint)}) {
             ${css(...styles)}
         }
     `;
 
-    const greaterThan = (breakpoint) => (...styles) => css`
+  const greaterThan = (breakpoint) => (...styles) => css`
         @media (min-width: ${getSizeFromBreakpoint(breakpoint)}) {
             ${css(...styles)}
         }
     `;
 
-    const between = (minBreakpoint, maxBreakpoint) => (...styles) => css`
+  const between = (minBreakpoint, maxBreakpoint) => (...styles) => css`
         @media (min-width: ${getSizeFromBreakpoint(minBreakpoint)}) and
         (max-width: ${getSizeFromBreakpoint(maxBreakpoint)}) {
         ${css(...styles)}
         }
     `;
 
-    return {
-        lessThan,
-        greaterThan,
-        between,
-    };
+  return {
+    lessThan,
+    greaterThan,
+    between,
+  };
 }
 
 export default mediaQueries;
