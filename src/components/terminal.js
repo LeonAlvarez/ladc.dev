a3b0a7b312aeb9348b365d11cda1d344fa0208d0
Leon Alvarez Del Canto <leon@leon-design.es>
2020-07-12 14:00:55 +0200

    Who Im

--- src/components/terminal.js
+++ src/components/terminal.js
@@ -9,6 +9,7 @@ const TerminalWrapper = styled.section`
   border-radius: 10px;
   margin: auto;
   position: relative;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
 `;
 
 const TerminalHeader = styled.header`
@@ -18,9 +19,9 @@ const TerminalHeader = styled.header`
   padding-left: .8rem;
 `;
 const iconsColors = {
-  green: '#51d88a',
-  yellow: '#fff382',
-  red: '#ef5753',
+  green: '#48bb78',
+  yellow: '#fbd38d',
+  red: '#f56565',
 };
 
 const TerminalHeaderIcon = styled.div`
@@ -36,12 +37,13 @@ const TerminalBody = styled.section`
   color: #fff;
   font-family: Operator Mono,Fira Code,Menlo,Consolas,Monaco,Andale Mono,Courier New,Courier;
   font-size: 1rem;
-  background: #30353a;
+  background: #151c23;
   padding: 1rem;
   box-sizing: border-box;
   position: absolute;
   width: 100%;
   top: 30px;
+  border-radius: 0 0 10px 10px;
   bottom: 0;
   opacity: .85;
   overflow: auto;
@@ -64,7 +66,7 @@ const TerminalCursor = styled.span`
   }
 `;
 
-const Terminal = ({ className }) => (
+const Terminal = ({ className, children }) => (
   <TerminalWrapper className={className}>
     <TerminalHeader>
       <TerminalHeaderIcon color="green" />
@@ -78,6 +80,7 @@ const Terminal = ({ className }) => (
         <TerminalCursor />
       </div>
     </TerminalBody>
+    {children}
   </TerminalWrapper>
 )
 
--- src/components/whoIm.js
+++ src/components/whoIm.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 import styled from 'styled-components';
-
-import { Container, Heading } from '../utils/components';
+import { Github, Twitter, Telegram } from '@styled-icons/fa-brands'
+import { AlternateEmail } from '@styled-icons/material';
+import { Container, Heading, Button } from '../utils/components';
 import Terminal from './terminal';
 
 const WhoImWrapper = styled.section`
@@ -36,20 +37,43 @@ const ImBgtitle = styled.h2`
   opacity: .1;
 `;
 
+const WhoImTextWrapper = styled.div`
+
+`;
+
 const WhoImTitle = styled(Heading)`
   opacity: 0.75;
   color: red;
 `;
 
+const TerminalSocial = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  right: -3rem;
+  top: 1rem;
+  height: 12rem;
+  justify-content: space-around;
+`;
+
 const WhoIm = () => (
   <WhoImWrapper>
     <WhoImContainer>
-      <Heading level={4}>Now you may be wondering..</Heading>
-      <WhoImTitle level={3} fontSize="4rem">Who Are You?</WhoImTitle>
-      <button>
-        More about me
-        </button>
-      <WhoImTerminal />
+      <WhoImTextWrapper>
+        <Heading level={4}>Now you may be wondering..</Heading>
+        <WhoImTitle level={3} fontSize="4rem">Who Are You?</WhoImTitle>
+        <Button primary rounded>
+          About me
+        </Button>
+      </WhoImTextWrapper>
+      <WhoImTerminal>
+        <TerminalSocial>
+          <Github size="32" color="white" title="Github" />
+          <AlternateEmail size="32" color="#d42c2c" title="Email" />
+          <Twitter size="32" color="rgba(29,161,242,1.00)" title="Twitter" />
+          <Telegram size="32" color="#179cde" title="Telegram" />
+        </TerminalSocial>
+      </WhoImTerminal>
       <ImBgtitle>LEÓN</ImBgtitle>
     </WhoImContainer>
   </WhoImWrapper>
