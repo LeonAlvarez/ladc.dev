8c2f40f4338f1c41fbfb05b1fc00f24fbedbd4c3
Leon Alvarez Del Canto <leon@leon-design.es>
2020-07-13 01:07:58 +0200

    remove unused hover

--- src/components/terminal.js
+++ src/components/terminal.js
@@ -34,11 +34,6 @@ const TerminalWrapper = styled.section`
   margin: auto;
   position: relative;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
-  &:hover {
-     ${TerminalBody} {
-      opacity: 1;
-    }
-  }
 `;
 
 const TerminalHeader = styled.div`
