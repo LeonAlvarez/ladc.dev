9cb6eb88e1abf837f3f83b9015bf732fe4dedb47
Leon Alvarez Del Canto <leon@leon-design.es>
2020-07-12 19:51:07 +0200

    hotfix

--- src/components/terminal.js
+++ src/components/terminal.js
@@ -3,23 +3,21 @@ import styled from 'styled-components';
 import useFullscreen from "@am-hooks/use-full-screen";
 
 const replaceCaret = (el) => {
-  if (window) {
-    // Place the caret at the end of the element
-    const target = document.createTextNode('');
-    el.appendChild(target);
-    // do not move caret if element was not focused
-    const isTargetFocused = document.activeElement === el;
-    if (target !== null && target.nodeValue !== null && isTargetFocused) {
-      var sel = window.getSelection();
-      if (sel !== null) {
-        var range = document.createRange();
-        range.setStart(target, target.nodeValue.length);
-        range.collapse(true);
-        sel.removeAllRanges();
-        sel.addRange(range);
-      }
-      if (el instanceof HTMLElement) el.focus();
+  // Place the caret at the end of the element
+  const target = document.createTextNode('');
+  el.appendChild(target);
+  // do not move caret if element was not focused
+  const isTargetFocused = document.activeElement === el;
+  if (target !== null && target.nodeValue !== null && isTargetFocused) {
+    var sel = window.getSelection();
+    if (sel !== null) {
+      var range = document.createRange();
+      range.setStart(target, target.nodeValue.length);
+      range.collapse(true);
+      sel.removeAllRanges();
+      sel.addRange(range);
     }
+    if (el instanceof HTMLElement) el.focus();
   }
 }
 
@@ -119,7 +117,7 @@ const TerminalLine = ({ innerRef, executeCommand }) => {
       el.innerHTML = html;
     }
     setLastHtml(html);
-    //if (window) replaceCaret(el);
+    replaceCaret(el);
   });
 
   return (
